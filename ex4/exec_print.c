24396f50c7114ccbe38541060d611f848e455991
Moin Uddin <U_Moin75@yahoo.com>
2019-03-20 20:58:14 -0400

    Complete ex4

---
+++ ex4/exec_print.c
@@ -0,0 +1,13 @@
+//
+// Created by Moin Uddin on 2019-03-20.
+//
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    printf("exec_print Executed!\n");
+
+    return 0;
+}
---
+++ ex4/exec_print2.c
@@ -0,0 +1,13 @@
+//
+// Created by Moin Uddin on 2019-03-20.
+//
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    printf("exec_print2 Executed!\n");
+
+    return 0;
+}
